3e7748baad6dcb7c94b811c39e790226cd8ff3a0
Ramim Mahabub <karosh498@gmail.com>
2026-05-14 06:21:09 +0600

    sdsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,7 +119,7 @@ export default async function Home() {
         <Testimonials />
 
         {/* Instagram Gallery */}
-        <InstagramSection />
+        {false && <InstagramSection />}
       </main>
       <Footer />
     </>
